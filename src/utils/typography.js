01c95130f5d44a9b48985f22cf3513aa64c15b0a
Timo Kirkkala <timo.kirkkala@wunder.io>
2018-08-08 10:05:26 +0300

    Add moraga theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,14 @@
 import Typography from "typography";
 
-const typography = new Typography({ baseFontSize: "22px" });
+import moragaTheme from "typography-theme-moraga";
+
+// Other theme packages available:
+import bootstrapTheme from "typography-theme-bootstrap";
+import twinPeaksTheme from "typography-theme-twin-peaks";
+//import lawtonTheme from "typography-theme-lawton";
+
+// Use readymade typography themes.
+const typography = new Typography(moragaTheme);
+
 
 export default typography;
